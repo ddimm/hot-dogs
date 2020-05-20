cfee567bc9496b46cc97e56f11ada5c2e975a2d6
Rebecca Zhou <rmz6yx@virginia.edu>
2020-05-20 10:45:44 -0400

    configure grommet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
+import Button from './components/Button';
+import Card from './components/Card'
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card />
+      <Button />
     </div>
   );
 }
